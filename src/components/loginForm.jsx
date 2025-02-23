fd8d1aece83eef86e604e26465c02941e3fa1205
Julio <juliomontesinosalvarado@gmail.com>
2025-02-23 21:30:36 +0100

    Typing Error fixed

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import SimpleMessageBar from "./SimpleMessageBar";
-import "../styles/LoginForm.css";
+import "../styles/loginForm.css";
 
 const LoginForm = () => {
     const [formData, setFormData] = useState({ email: "", password: "" });
